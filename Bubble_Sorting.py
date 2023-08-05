1e42545852af43ce60028b58c0a47225a0a8801b
aliarslan2 <141162785+aliarslan2@users.noreply.github.com>
2023-08-05 18:53:34 +0300

    Update Bubble_Sorting.py

--- Bubble_Sorting.py
+++ Bubble_Sorting.py
@@ -1,23 +1,21 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Aug  5 18:08:03 2023
 
-@author: arsla
 """
-def bubbleSort(array):
+def BubbleSort(array):
     lastindex=len(array)
     print("Our array is ",array)
     
     
-    for i in range(lastindex-1):
+    for i in range(lastindex-1):       # We repeat the process len(array) times
         index1=0
         index2=1
         
     
-        while index2<lastindex:
+        while index2<lastindex:        # We want to compare each of the two elements of the set till the very end.
             
         
-            if(array[index1]>array[index2]):
+            if(array[index1]>array[index2]):            # We check the two values and switch them if they are not in order
                 
                 switch1=array[index1]
                 switch2=array[index2]
@@ -37,4 +35,4 @@ for i in range(0,8):
     n= int(input("Enter a value of an array containing 8 integer values: "))
     array[i]+=n
     
-print("Our sorted array is ",bubbleSort(array))
+print("Our sorted array is ",BubbleSort(array))
