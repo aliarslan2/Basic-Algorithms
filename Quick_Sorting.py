28faede59064ea347931c9dbfdda2f30320df7fe
aliarslan2 <141162785+aliarslan2@users.noreply.github.com>
2023-08-05 18:48:25 +0300

    Update Quick_Sorting.py

--- Quick_Sorting.py
+++ Quick_Sorting.py
@@ -1,17 +1,14 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Aug  5 17:44:29 2023
-
-@author: arsla
 """
 
 def QuickSorting(array):
     
-    print("Our array is ",array)
+    print("Our array is ",array)    #Original Array
     
-    for pivot in range (len(array)):
-        left=0
-        right= len(array)-1
+    for pivot in range (len(array)):            # We repeat the process by taking all of the elements as a pivot one by one
+        left=0                #Left index of the array
+        right= len(array)-1     #Right index of the array
      
         while   right>left:
     
@@ -22,10 +19,11 @@ def QuickSorting(array):
                 switch2=array[left]
                 
                 array[left]=switch1
-                array[right]=switch2
+                array[right]=switch2        #Switching the array elements in case there is a misorder
     
-            left+=1
-            if (left==pivot):
+            left+=1                        # We check if any of the elements on the left is greater than that on the right  
+            if (left==pivot):                #When we reach the pivot, we push back the left index and move one step back our right index
+                                            # and repeat the process until the left and the right side of the pivot is sorted
                 left=0
                 right-=1
         
@@ -33,7 +31,7 @@ def QuickSorting(array):
                 
 array=[0,0,0,0,0,0,0,0]
 
-for i in range(0,8):
+for i in range(0,8):                        # Our array consists of 8 elements and we take the elements from the user
     
     n= int(input("Enter a value of an array containing 8 integer values: "))
     array[i]+=n
